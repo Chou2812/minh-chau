6f337945f56f5ded690c6217bdb546e4c5c82f5c
Chau Pham <chaump.cheil@gmail.com>
2024-04-22 21:13:10 +0700

    add source code

---
+++ project/scheme/notebook.py
@@ -0,0 +1,45 @@
+"""
+    Đề án 1:
+    Tạo tool giúp đăng nhập tự động cho trang CMS mà không cần xài chuột (tham khảo lại có nên đổi thành đề án 2 hay không)
+    
+    Đề án 2: 
+    Viết 1 tool có thể lấy được tất cả data trên 1 website tĩnh về máy. Sau đó lọc ra được 1 file ALT text
+    của tất cả các hình, video trên trang đó
+    
+    Đề án 3: 
+    Tạo 1 game tên "hôm nay ăn gì" trên 1 website
+    Gameplay: Cho phép user có thể input danh sách món ăn
+    Hệ thống trả về 1 kết quả gợi ý ngẫu nhiên
+    Trong trường hợp user chưa có danh sách để input, có thể sử dụng tính năng gợi ý của hệ thống, hệ thống dựa vào
+    1 danh sách thư viện đã có sẵn để trả 1 kết quả ngẫu nhiên
+    Danh sách người dùng input vào sẽ được thu thập vào thư viện của hệ thống để sử dụng cho lần tiếp theo
+    """
+    
+    # Ý tưởng triển khai đề án
+    
+"""
+    Đề án 1: Đang tham khảo lại
+    
+    Đề án 2: Chưa biết phải làm sao
+    
+    
+    Đề án 3:
+    Có 3 phần chính:
+    1. Sử dụng python để viết cho phần backend
+        Đinh nghĩa tính năng: 
+            * Cho người dùng nhập vào 1 danh sách các món và trả về 1 món ăn ngẫu nhiên
+            * Cho phép sử dụng thư viện món ăn của hệ thống để lấy ra được tên 1 món ăn ngẫu nhiên
+            * Danh sách người dùng input vào sẽ được lấy làm dữ liệu đẩy vào file thư viện món ăn của hệ thống (file cvs)
+        Mục tiêu tính năng:
+            * Đưa ra gợi ý, giải quyết vần đề của người dùng trong việc lựa chọn món ăn
+            * Lấy dữ liệu từ người dùng để mở rộng data vốn có của mình, phục vụ cho những lần gợi ý tiếp theo ngày càng đa dạng hơn
+            * Nền tảng để tạo ra những game có cách chơi tương tự nhưng với nội dung khác
+    2. Sử dụng HTML + CSS để thiết kế giao diện web
+        Thiết kế giao diện (UX, UI) -  chưa biết, cần học
+        Xử lý UX - UI - chưa biết, cần học
+    
+    3. Giao tiếp giữa frontend và backend: Viết API dựa trên thư viện flask
+        Viết API: Dựa vào thư viện Flask viết ra 1 API cho game - chưa biết, cần học
+    nhớ hỏi thêm thầy để xin gợi ý
+    """
+    
